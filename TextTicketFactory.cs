3558d5cfca83b46f9673fd743fcb2da69618e2a4
lenok-ar <leno4ka279a@gmail.com>
2025-04-26 01:49:37 +0700

    init project

---
+++ TextTicketFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsFormsApp1
+{
+  public class TextTicketFactory : ITicketFactory
+  {
+    public ITicket CreateTicket(string ticketDetails)
+    {
+      return new TextTicket(ticketDetails);
+    }
+  }
+}
