7089bf8e48260c4a4274046a196fc25b7b849b7c
lenok-ar <leno4ka279a@gmail.com>
2025-04-26 01:49:14 +0700

    init project

---
+++ TextTicket.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace WindowsFormsApp1
+{
+  public class TextTicket : ITicket
+  {
+    private string _ticketDetails;
+
+    public TextTicket(string ticketDetails)
+    {
+      _ticketDetails = ticketDetails;
+    }
+
+    public string GetTicketDetails()
+    {
+      return _ticketDetails;
+    }
+
+    public void SaveTicket(string filePath)
+    {
+      File.WriteAllText(filePath, _ticketDetails);
+    }
+
+    public void DeleteTicket(string filePath)
+    {
+      if (File.Exists(filePath))
+      {
+        File.Delete(filePath);
+      }
+      else
+      {
+        throw new FileNotFoundException("Файл не найден для удаления: " + filePath);
+      }
+    }
+  }
+}
